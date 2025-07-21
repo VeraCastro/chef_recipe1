3592a316c7fca5435f8901e2a7aff1fd754fc392
VeraCastro <vmcastro82@gmail.com>
2025-07-20 20:41:54 -0600

    Update default.rb

--- default.rb
+++ default.rb
@@ -1,48 +1,39 @@
 #
-# Cookbook:: actividad2
+# Cookbook:: mongodb
 # Recipe:: default
 #
 # Copyright:: 2025, The Authors, All Rights Reserved.
-#package 'apache2' do  
-#  action :install  
-#end  
+  execute 'install gnupg and curl' do
+    command 'apt-get install -y gnupg curl'
+    action :run
+  end
   
-#service 'apache2' do  
-#  action [:enable, :start]  
-#end
-
-
-execute 'install gnupg and curl' do
-  command 'apt-get install -y gnupg curl'
+  # Agregar la clave GPG de MongoDB
+  execute 'add mongodb gpg key' do
+    command 'curl -fsSL https://www.mongodb.org/static/pgp/server-6.0.asc | \
+     gpg -o /usr/share/keyrings/mongodb-server-6.0.gpg --dearmor'
+    action :run
+    not_if { ::File.exist?('/usr/share/keyrings/mongodb-server-6.0.gpg') }
+  end
+  
+  # Agregar el repositorio de MongoDB
+execute 'add mongodb repository' do
+  command 'echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-6.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-6.0.list'
   action :run
-end
-
-execute 'add mongodb gpg key' do 
-  command 'curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
-   --dearmor'
-  action:run
-  not_if { ::File.exist?('/usr/share/keyrings/mongodb-server-8.0.gpg') } # Evita repetir si ya exist
-end 
-
-# Agregar el repositorio de MongoDB  
-file '/etc/apt/sources.list.d/mongodb-org-8.0.list' do
-  content 'deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/8.0 multiverse'
-  mode '0644'
-  action :create
-end
-
-execute 'apt-get update' do
-  command 'apt-get update'
-  action :run
-end
-
-# Instalar MongoDB  
-package 'mongodb-org' do  
-  action :install  
+  not_if { ::File.exist?('/etc/apt/sources.list.d/mongodb-org-6.0.list') }
 end  
+  # Actualizar los repositorios
+  execute 'update apt repositories' do
+    command 'apt-get update'
+    action :run
+  end
   
-# Habilitar y arrancar el servicio de MongoDB  
-service 'mongod' do  
-  action [:enable, :start]  
-end
+  # Instalar MongoDB
+  package 'mongodb-org' do
+    action :install
+  end
+  
+  # Habilitar y arrancar el servicio de MongoDB
+  service 'mongod' do
+    action [:enable, :start]
+  end
